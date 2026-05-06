960f238ab0970e739de90d3177468fc1a838cd9a
朱雅玲 <zhuyaling@zhuyalingdeMacBook-Air.local>
2026-05-06 11:15:27 +0800

    chore: update Meta Pixel ID to 874127278420044

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -54,13 +54,13 @@ export default function RootLayout({
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '938297802395453');
+            fbq('init', '874127278420044');
             fbq('track', 'PageView');
           `}
         </Script>
         <noscript>
           <img height="1" width="1" style={{display:'none'}}
-            src="https://www.facebook.com/tr?id=938297802395453&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=874127278420044&ev=PageView&noscript=1"
           />
         </noscript>
         {/* End Meta Pixel Code */}
